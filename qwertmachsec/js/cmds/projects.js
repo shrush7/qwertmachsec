60fd625d38bfd8dd1342cda18c341916946e71e1
Shrushti Patil <shrushpatil111@gmail.com>
2024-08-21 18:01:32 +0530

    Add files via upload

--- qwertmachsec/js/cmds/projects.js
+++ qwertmachsec/js/cmds/projects.js
@@ -5,18 +5,29 @@ export default function(output, _hist, ...args) {
         case 'qwertmachsec':
             window.open('https://github.com/shrush7/qwertmachsec');
             break;
-        case 'CODSOFT':
-            window.open('https://github.com/shrush7/CODSOFT');
+        case 'insecure_xss':
+            window.open('https://github.com/shrush7/insecure_xss');
+            break;
+        case 'arbitary_code_execution':
+            window.open('https://github.com/shrush7/Arbitary_code_execution');
+            break;
+        case 'exposed_phpinfo':
+            window.open('https://github.com/shrush7/exposed_phpinfo');
+            break;
+        case 'PHP_web_shell':
+            window.open('https://github.com/shrush7/PHP_web_shell');
             break;
         case 'nodekeylogger':
             window.open('https://github.com/shrush7/nodekeylogger')
         default:
             output.innerHTML += `
 <span class="asdf"data-color="lime"><a href="https://github.com/shrush7/qwertmachsec"target="_blank">qwertmachsec</a>: This wesbite.</span>
-<span class="asdf"data-color="lime"><a href="https://github.com/shrush7/CODSOFT"target="_blank">codsoft</a>: Data Science projects that I did in my internship.</span>
+<span class="asdf"data-color="lime"><a href="https://github.com/shrush7/insecure_xss"target="_blank">insecure_xss</a>: A deliberately vulnerable web application with insecure XSS filters.</span>
+<span class="asdf"data-color="lime"><a href="https://github.com/shrush7/Arbitary_code_execution"target="_blank">arbitary_code_execution</a>: A vulnerable web application that is susceptible to arbitrary command execution.</span>
+<span class="asdf"data-color="lime"><a href="https://github.com/shrush7/exposed_phpinfo"target="_blank">exposed_phpinfo</a>: A simple PHP web application with an exposed php.info page.</span>
+<span class="asdf"data-color="lime"><a href="https://github.com/shrush7/PHP_web_shell"target="_blank">PHP_web_shell</a>: A PHP backdoor accessible via your browser, that allows you to execute malicious commands on the compromised web server.</span>
 <span class="asdf"data-color="lime"><a href="https://github.com/shrush7/nodekeylogger"target="_blank">nodekeylogger</a>: A simple linux node keylogger.</span>
-
-(tip: click the project title to visit it's github page)`;
+`;
         return;
     }
 }
--- qwertmachsec/js/cmds/whoami.js
+++ qwertmachsec/js/cmds/whoami.js
@@ -3,7 +3,7 @@ export default function(output) {
 I am an Artificial Intelligence and Data Science student with a keen interest in Cybersecurity and Open-source Software. 
 I actively participate in Capture The Flag (CTF) competitions, engage with Hack The Box labs, and keep up with emerging threats and technologies in the field. 
 In my free time, I delve into computer hardware, code, and explore new tools and technologies. 
-I use Kali Linux as my daily driver and my hobbies include playing sports, video games, and delving into the realm of quantum physics.
+I use Linux as my daily driver and my hobbies include playing sports, video games, and delving into the realm of quantum physics.
  
      _______||_______
     /   _             \\
